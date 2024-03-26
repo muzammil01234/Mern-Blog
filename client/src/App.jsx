1662caff1a62ab0bad4f3b6e84fd9154cf91aa33
Muzammil01234 <21je0862@iitism.ac.in>
2024-03-26 17:38:19 +0530

    Iniitialised my git

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500">
+        Hi I'm working with tailwind css
+      </h1>
+    </div>
+  );
+}
