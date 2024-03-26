c156e245614efc7f6f9b47c18f7245e07bbf9ffc
Muzammil01234 <21je0862@iitism.ac.in>
2024-03-26 23:11:45 +0530

    Added Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Signin from "./pages/About.jsx";
+import Signup from "./pages/About.jsx";
+import Dashboard from "./pages/Dashbord.jsx";
+import Projects from "./pages/Projects.jsx";
+import Header from "./components/Header.jsx";
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">
-        Hi I'm working with tailwind css
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/Signin" element={<Signin></Signin>}></Route>
+        <Route path="/Signup" element={<Signup></Signup>}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/project" element={<Projects></Projects>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+export default function Header() {
+  const location = useLocation();
+  return (
+    <>
+      <Navbar className="border-b-2 ">
+        <Link
+          to="/"
+          className="whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Muzzu's
+          </span>
+          Blog
+        </Link>
+        <form>
+          <TextInput
+            type="text"
+            placeholder="Enter text to Search"
+            rightIcon={AiOutlineSearch}
+            className=" hidden lg:inline"
+          ></TextInput>
+        </form>
+        <Button className="w-12 h-10 bg-gray-500 rounded-2xl lg:hidden" pill>
+          <AiOutlineSearch />
+        </Button>
+        <div className="flex  gap-2 md:order-2">
+          <Button className="w-12 h-10 " color="gray" pill>
+            <FaMoon />
+          </Button>
+          <Link to="/signin">
+            <Button gradientDuoTone="purpleToBlue" className=" text-xl">
+              SignIn
+            </Button>
+          </Link>
+        </div>
+        <Navbar.Toggle></Navbar.Toggle>
+        <Navbar.Collapse>
+          <Navbar.Link active={location.pathname == "/"} as={div}>
+            <Link className=" text-xl" to="/">
+              Home
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={location.pathname == "/about"} as={div}>
+            <Link to="/about" className=" text-xl">
+              About
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={location.pathname == "/project"} as={div}>
+            <Link to="/project" className=" text-xl">
+              Projects
+            </Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
