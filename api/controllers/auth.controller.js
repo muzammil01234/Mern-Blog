c3e93724e30961adf923b4777ead79a071936605
Muzammil01234 <21je0862@iitism.ac.in>
2024-03-27 16:43:57 +0530

    Added Google OAuth functionallity

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,5 +44,36 @@ const signin=async (req,res,next)=>{
           next(errorHandler(500,err.message));
      }
 }
+const google =async (req,res,next)=>{
+    let {email,username,photoURL}=req.body;
+    console.log(req.body);
+    try{
+     const user=await User.findOne({email})
+     if(user){
+          const token=jwt.sign({id:user._id},process.env.JWT_SIGN);
+          res.status(200).cookie('auth_token',token,{httpOnly:true}).json(user);
+     }
+     else{
+          const password=Math.random().toString(36).slice(-8);
+          const hashpass=bcryptjs.hashSync(password,10);
+          username=username.split(' ').join('');
+          const newUser =new User({
+              email,
+              username,
+              password,
+              profilePhoto:photoURL
+              
+          })
+          let user=await    newUser.save();
+          const token=jwt.sign({id:user._id},process.env.JWT_SIGN);
+          res.status(200).cookie('auth_token',token,{httpOnly:true}).json(user);
+
+     }
+    
+    }
+    catch(error){
+     next(error);
+}
+}
 export default signup;
-export {signin};
+export {signin,google};
